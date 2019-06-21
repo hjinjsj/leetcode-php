2c0ea4f53f980d0f948668a4f2e223fa30ecfc4f
huangjin <huangj@xiaopeng.com>
2019-06-21 10:46:38 +0800

    19

---
+++ 19.php
@@ -0,0 +1,54 @@
+<?php
+//给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+//
+//示例：
+//
+//给定一个链表: 1->2->3->4->5, 和 n = 2.
+//
+//当删除了倒数第二个节点后，链表变为 1->2->3->5.
+//说明：
+//
+//给定的 n 保证是有效的。
+//
+//进阶：
+//
+//你能尝试使用一趟扫描实现吗？
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @param Integer $n
+     * @return ListNode
+     */
+    function removeNthFromEnd($head, $n) {
+        $res = $head;
+        $rNode = $head;
+        $pos = 0;
+        while ($head) {
+            $pos ++;
+            if ($pos > $n + 1) {
+                $rNode = $rNode->next;
+            }
+            $head = $head->next;
+        }
+        if ($n == $pos) {
+            return $res->next;
+        } else {
+            $rNode->next = $rNode->next->next;
+        }
+        return $res;
+    }
+}
